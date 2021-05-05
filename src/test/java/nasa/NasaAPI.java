d0379c1992b0935f4a304d82262c438bc07a92c6
Rodrigo Ayala <rodrigo.ayala@unosquare.com>
2021-05-05 08:09:24 -0500

    pojo classes added

--- src/test/java/nasa/NasaAPI.java
+++ src/test/java/nasa/NasaAPI.java
@@ -1,15 +1,13 @@
 package nasa;
 
-import static org.testng.Assert.assertEquals;
-
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-import utils.JsonObject;
+import io.restassured.parsing.Parser;
+import utils.NasaJSON;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -30,24 +28,40 @@ public class NasaAPI {
 	
 	@BeforeClass
 	public void setUp() {
-			JsonObject jo = new JsonObject();
-			jo.setUrl("https://api.nasa.gov");
-			jo.setApi_key("Rgr0tol175zyNjjLahTVwJB7oPKJSyqbpBIpcxcI");
-			
+		RestAssured.baseURI = "https://api.nasa.gov";
 	}
 	
 	@Test
-	public void firstTest() {
-		RestAssured.baseURI = "https://api.nasa.gov";
+	public void getResponse200() {
 		String response = given().log().all().queryParam("api_key", "Rgr0tol175zyNjjLahTVwJB7oPKJSyqbpBIpcxcI").header("Content-Type", "application/json")
 		.when().get("planetary/apod")
-		.then().assertThat().statusCode(200).body("date", equalTo("2021-05-04")).extract().response().asString();
+		.then().assertThat().statusCode(200).body("date", equalTo("2021-05-05")).extract().response().asString();
 		
-		System.out.println(response);
+		System.out.println("The response is empty? " + response.isEmpty());
+		Assert.assertFalse(response.isEmpty(), "The response is empty");
 		
-		JsonPath js = new JsonPath(response);
-		String cr = js.getString("copyright");
-		System.out.println(cr);
+//		System.out.println(response);
+//		JsonPath js = new JsonPath(response);
+//		String cr = js.getString("copyright");
+//		System.out.println(cr);
+		
+	}
+	
+	@Test
+	public void getResponseDeserialization() {
+		RestAssured.baseURI = "https://api.nasa.gov";
+		NasaJSON nasajson = given().queryParam("api_key", "Rgr0tol175zyNjjLahTVwJB7oPKJSyqbpBIpcxcI").header("Content-Type", "application/json")
+				.expect().defaultParser(Parser.JSON)
+		.when().get("planetary/apod").as(NasaJSON.class);
+		
+		System.out.println(nasajson.getCopyright());
+		System.out.println(nasajson.getDate());
+		System.out.println(nasajson.getExplanation());
+		System.out.println(nasajson.getHdurl());
+		System.out.println(nasajson.getMedia_type()); 
+		System.out.println(nasajson.getService_version());
+		System.out.println(nasajson.getTitle());
+		System.out.println(nasajson.getUrl());
 	}
 	
 	@AfterClass
